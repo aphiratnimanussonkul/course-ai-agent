818cfc7861bacc7646487138fd416b30d4f549cf
Aphirat Nimanussonkul (Tata) <B5923151@gmail.com>
2026-06-17 22:01:34 +0700

    Update tasks.yaml and email_tool.py for improved email functionality and task descriptions
    - Enhanced task descriptions in tasks.yaml to specify that companies must be listed in the Stock Market.
    - Modified email_tool.py to send emails to a specified user email instead of a generic recipient, improving clarity in email notifications.

--- 5_crew_ai/src/stock_picker/tools/email_tool.py
+++ 5_crew_ai/src/stock_picker/tools/email_tool.py
@@ -11,7 +11,6 @@ dotenv.load_dotenv(override=True)
 class EmailMessage(BaseModel):
     """A message to be sent to the user"""
 
-    to: str = Field(..., description="The email address of the recipient.")
     subject: str = Field(..., description="The subject of the email.")
     body: str = Field(..., description="The body of the email.")
 
@@ -20,19 +19,22 @@ class EmailTool(BaseTool):
     mailer_send_api_token: str = os.getenv("MAILER_SEND_API_TOKEN")
     mailer_send_domain: str = os.getenv("MAILER_SEND_DOMAIN")
     ms: MailerSendClient = MailerSendClient(api_key=mailer_send_api_token)
+    your_email: str = os.getenv("YOUR_EMAIL")
 
     name: str = "Send an Email"
     description: str = "This tool is used to send an email to the user."
     args_schema: Type[BaseModel] = EmailMessage
 
-    def _run(self, to: str, subject: str, body: str) -> str:
-        print(f"Sending email to {to} with subject {subject} and body {body}")
+    def _run(self, subject: str, body: str) -> str:
+        print(
+            f"Sending email to {self.your_email} with subject {subject} and body {body}"
+        )
         email = (
             EmailBuilder()
-            .from_email(f"sales@{self.mailer_send_domain}", "Sales Agent")
-            .to_many([{"email": to}])
+            .from_email(f"stock_picker_agent@{self.mailer_send_domain}", "Stock Picker")
+            .to_many([{"email": self.your_email}])
             .subject(subject)
-            .body(body)
+            .text(body)
             .build()
         )
         response = self.ms.emails.send(email)
